05cfbd91f0cf26c0fea66ae47ccc4e6976174a15
thiagomoyses <thiagodasilvamoysesfr@gmail.com>
2023-09-19 15:54:44 +0200

    Save middleware to assure the request is a json

--- server.ts
+++ server.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import { routes } from "./src/routes";
+import { jsonValidator } from "./src/middlewares";
+
 
 const app = express();
 const PORT = 3000
 
 app.use(express.json());
+app.use(jsonValidator);
 app.use(routes);
 
 app.listen(PORT, () => {
