2931c5d97c46669552bb8aaf3c161e6491f4d215
thiagomoyses <thiagodasilvamoysesfr@gmail.com>
2023-09-20 16:34:02 +0200

    login logic

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,10 +1,14 @@
 import { Request, Response } from "express";
 import { AuthService } from "./auth.service";
+import { AuthDto } from "./dto";
 
 export class AuthController {
     async login(req: Request, res: Response){
+        
         const authService = new AuthService();
+        const payload: AuthDto = req.body
+        const tryLogin = await authService.login(payload);
 
-        return res.send(authService.login());    
+        return res.status(tryLogin.status).json(tryLogin);
     }
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,5 +1,28 @@
+import { prisma } from "../../prisma/client";
+import { Responses } from "../../responses/responses";
+import { AuthDto } from "./dto";
+import * as argon from 'argon2';
+
 export class AuthService {
-    login(){
-        return "Chego tambem";
-    }   
+    private responses = new Responses();
+
+    async login({ email, password }: AuthDto) {
+
+        try {
+            const findUser = await prisma.user.findUnique({
+                where: {
+                    email: email
+                }
+            });
+
+            if (!findUser) return this.responses.sendResponse(false, "Wrong credentials!", null, 401);
+
+            const pwMatches = await argon.verify(findUser.hash, password);
+            if (!pwMatches) return this.responses.sendResponse(false, "Wrong credentials!", null, 401);
+
+            return this.responses.sendResponse(true, "User loged!", null, 200);
+        } catch (error) {
+            return this.responses.sendResponse(false, "Internal error!", null, 500);
+        }
+    }
 }
