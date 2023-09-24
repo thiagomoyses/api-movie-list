b40624a001ea2c608984fb5673a838a3991f0b46
thiagomoyses <thiagodasilvamoysesfr@gmail.com>
2023-09-24 14:22:44 +0200

    delete useless code

--- src/validator/schema/movie.schema.ts
+++ src/validator/schema/movie.schema.ts
@@ -4,5 +4,4 @@ export const movieSchema = joi.object({
     title: joi.string().required(),
     category: joi.string().required(),
     status: joi.string().required(),
-    user_reff: joi.string().required(),
 });
